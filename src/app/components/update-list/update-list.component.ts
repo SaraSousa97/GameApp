35d97821953d656c309b7cfb612c51eb4ddcd348
Sara Sousa <sara.sousa.natixis@msft.cesae.pt>
2024-11-11 11:49:15 +0000

    pages + interfaces

---
+++ src/app/components/update-list/update-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-list.component.html',
+  styleUrl: './update-list.component.scss'
+})
+export class UpdateListComponent {
+
+}
