a09bed5624b8f12b856d863c360f7ba5e79363b3
Sara Sousa <sara.sousa.natixis@msft.cesae.pt>
2024-11-11 13:01:51 +0000

    gamelist

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { ListsService } from '../../services/lists.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+  games: Array<any> = [];
+
+  constructor(private listService: ListsService){}
+
+  ngOnInit(){
+
+    this.listService.getGamesList().subscribe({
+      next: (data) => {
+        console.log(data);
+        this.games = data;
+      },
+      error: (error) =>{
+        console.log('Algo correu mal: ', error);
+      }
+    });
+  }
 }
