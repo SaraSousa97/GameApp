442635717d4b05e7529bc6d9c5abcbf13b9bcb3d
carol <csimoes0507@gmail.com>
2024-11-15 11:47:52 +0000

    game details page working

--- src/app/pages/game-details/game-details.component.ts
+++ src/app/pages/game-details/game-details.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { GameInfo } from '../../models/game-info';
+import { ListsService } from '../../services/lists.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-game-details',
@@ -8,5 +11,25 @@ import { Component } from '@angular/core';
   styleUrl: './game-details.component.scss'
 })
 export class GameDetailsComponent {
+
+  gameInfo: GameInfo | undefined;
+
+
+  constructor(private route: ActivatedRoute, private listsService:ListsService, private router: Router){}
+
+
+ 
+  ngOnInit() {
+    const id= this.route.snapshot.paramMap.get('id') || '1';
+    this.listsService.getGamesInfo(id).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.gameInfo = data;
+      },
+      error: (error) => {
+        console.log('Something went wrong: ', error);
+      }
+    });
+  }
 
 }
--- src/app/services/lists.service.ts
+++ src/app/services/lists.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Game } from '../models/game';
+import { GameInfo } from '../models/game-info';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,10 @@ export class ListsService {
   constructor(private http: HttpClient) {}
 
   getGamesList(): Observable<Game[]> {
-    return this.http.get<Game[]>('http://localhost:3000/gamesList');  // Replace 'api/games' with your actual endpoint
+    return this.http.get<Game[]>('http://localhost:3000/gamesList'); 
+  }
+
+  getGamesInfo(id: string):Observable<GameInfo>{
+    return this.http.get<GameInfo>('http://localhost:3000/gameDetails/' + id); 
   }
 }
