fbf99af34feb01169c1f453b63e20b637bfbd1f3
Sara Sousa <sara.sousa.natixis@msft.cesae.pt>
2024-11-16 16:59:51 +0000

    userservice

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,21 +1,14 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-<<<<<<< HEAD
 import { List, User } from '../models/user';
 import { Observable } from 'rxjs/internal/Observable';
 import { HttpClient } from '@angular/common/http';
 import { GameInfo } from '../models/game-info';
-=======
-import { User } from '../models/user';
-import { Observable } from 'rxjs';
->>>>>>> 4760a5641a35854681a6ee83f411ef8e3bf18f4c
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-<<<<<<< HEAD
   constructor(private http: HttpClient) { }
 
   getUser(): Observable<User>{
@@ -25,16 +18,6 @@ export class UserService {
   getGameImage(gameId: string): Observable<GameInfo> {
     return this.http.get<GameInfo>(`http://localhost:3000/games/${gameId}`);
   }
-=======
-  user: User | undefined;
-
-  constructor(private http: HttpClient) {}
-
-  getUser(id: string): Observable<User> {
-    return this.http.get<User>('http://localhost:3000/profile/'); 
-  }
-
->>>>>>> 4760a5641a35854681a6ee83f411ef8e3bf18f4c
 }
 
 
