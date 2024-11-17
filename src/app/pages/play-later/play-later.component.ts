a0d9a0b8461ccd90aa003982a89e14799ebd0b92
Sara Sousa <saradanielafs@hotmail.com>
2024-11-17 18:34:53 +0000

    play-later design update & alerta de remoção

--- src/app/pages/play-later/play-later.component.ts
+++ src/app/pages/play-later/play-later.component.ts
@@ -1,23 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { UserService } from '../../services/user.service';
 import { GameService } from '../../services/game.service';
 import { List, User } from '../../models/user';
 import { GameInfo } from '../../models/game-info';
 import { CommonModule } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar'; 
 
 @Component({
   selector: 'app-play-later',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './play-later.component.html',
   styleUrl: './play-later.component.scss'
 })
 export class PlayLaterComponent implements OnInit {
+  user: User | undefined;
   list: List | undefined;
   gameCache: { [gameId: string]: GameInfo } = {};
 
-  constructor(private userService: UserService, private gameService: GameService) {}
+  constructor(private userService: UserService, private gameService: GameService, private snackBar: MatSnackBar ) {}
 
   ngOnInit() {
     this.userService.getLists().subscribe({
@@ -44,5 +46,29 @@ export class PlayLaterComponent implements OnInit {
 
   getGameName(gameId: string): string {
     return this.gameCache[gameId]?.title || 'Unknown Game';
+  }
+
+  loadUserProfile() {
+    this.userService.getUser().subscribe(profile => {
+      this.user = profile;
+    });
+  }
+
+  removeGame(listId: string, gameId: string){
+    //remover o jogo da lista
+    this.userService.deleteGame(listId, gameId).subscribe({
+      next: () => {
+        this.loadUserProfile();
+        if (this.list) {
+          this.list.gamesIds = this.list.gamesIds.filter(id => id !== gameId);
+        }
+
+        this.snackBar.open('Game removed successfully!', 'Close', {
+          duration: 3000,
+          panelClass: ['snack-bar-success']
+        });
+      },
+      error: (error) => console.error('Erro ao remover jogo:', error)
+    });
   }
 }
