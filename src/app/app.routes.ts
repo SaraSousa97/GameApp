d0df9d19511c2082c6f187c6b01571ef81a0d736
Sara Sousa <saradanielafs@hotmail.com>
2024-11-17 15:53:45 +0000

    update ao app.routes -> adição dos novos componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,15 +4,21 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { UpdateUserComponent } from './components/update-user/update-user.component';
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 import { GameDetailsComponent } from './pages/game-details/game-details.component';
-import { UserListsComponent } from './pages/user-lists/user-lists.component';
+import { PlayLaterComponent } from './pages/play-later/play-later.component';
+import { PlayCurrentlyComponent } from './pages/play-currently/play-currently.component';
+import { PlayedComponent } from './pages/played/played.component';
+import { CompletedComponent } from './pages/completed/completed.component';
 
 export const routes: Routes = [
     {path:'home', component:HomeComponent},
     {path:'user-profile/:id',children:[
         {path:'',title:'User Profile', component:UserProfileComponent },
         {path:'update-user', title: 'Edit User Information',component:UpdateUserComponent},
-        {path:'my-lists', title: 'My Lists', component:UserListsComponent}]
-        },
+    ]},
+    {path:'play-later', title: 'Play Later', component:PlayLaterComponent},
+    {path:'play-currently', title: 'Play Currently', component:PlayCurrentlyComponent},
+    {path:'played', title: 'Played', component:PlayedComponent},
+    {path:'completed', title: 'Completed', component:CompletedComponent},
     {path:'game-details/:id', title: 'Game Details', component:GameDetailsComponent},
     {path:'', redirectTo:'/home', pathMatch:'full'},
     {path:'**', component:NotFoundComponent}
